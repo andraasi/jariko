75779139b46cb5ad5d33bf0f78c91fac196bb6c3
andreagosti9 <andrea.agosti9@gmail.com>
2024-07-17 08:58:34 +0200

    refactored

--- examples/src/main/kotlin/com/dspf/multithread/serialization.kt
+++ examples/src/main/kotlin/com/dspf/multithread/serialization.kt
@@ -19,45 +19,6 @@ import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.modules.contextual
 import kotlinx.serialization.modules.polymorphic
 import kotlinx.serialization.modules.subclass
-import java.net.Socket
-
-// using 'use' will also close the socket; use it wisely
-
-// saving readers and buffered is mandatory
-// because function creates a new one each time
-
-private const val EOB = "EOB"
-
-fun receive(socket: Socket): String {
-    println("receiving...")
-    val bufferedReader = socket.getInputStream().bufferedReader()
-    val string = StringBuilder()
-    var line: String?
-
-    // allows JSON to be collected and parsed correctly
-    while (bufferedReader.readLine().also { line = it } != null) {
-        if (line == EOB) {
-            break
-        }
-        string.append(line)
-    }
-
-    println("received ${string.length} B")
-    return string.toString()
-}
-
-fun send(socket: Socket, string: String) {
-    println("sending...")
-    val bufferedWriter = socket.getOutputStream().bufferedWriter()
-    bufferedWriter.write(string)
-    bufferedWriter.newLine()
-    bufferedWriter.write(EOB)
-    bufferedWriter.newLine()
-    bufferedWriter.flush()
-    println("sent ${string.length} B")
-}
-
-// serialization
 
 private val module = SerializersModule {
     contextual(BigDecimalSerializer)
@@ -93,4 +54,3 @@ val json = Json {
     serializersModule = module
     classDiscriminator = "json-type"
 }
-
